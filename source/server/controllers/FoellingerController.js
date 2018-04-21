39fb1ccf9fb6d53289c082bf9421e76fd44d186e
Shailpik Roy <sroy15@illinois.edu>
2018-04-21 18:23:12 -0500

    edited model

--- source/server/controllers/FoellingerController.js
+++ source/server/controllers/FoellingerController.js
@@ -15,11 +15,21 @@ exports.imageGetId = function (req, res) {
     if (err || !image)
       return res.status(404).send({ message: 'Error', data: 'Id not found'});
 
+    date = new Date(image.timestamp * 1000)
+    console.log(date.getDay())
+    console.log(date.getDate())
+    console.log(date.getMonth() + 1)
+    console.log(date.getFullYear())
     res.status(200).json({ message: 'OK', data: image});
   });
 }
 
 exports.imagePost = function (req, res) {
+  date = new Date(req.body.timestamp * 1000)
+  req.body.day = date.getDay()
+  req.body.date = date.getDate()
+  req.body.month = date.getMonth() + 1
+  req.body.year = date.getFullYear()
   var image = new Foellinger(req.body);
   image.save(function(err) {
     if (err)
--- source/server/models/foellinger.js
+++ source/server/models/foellinger.js
@@ -4,7 +4,11 @@ var mongoose = require('mongoose');
 // Define our user schema
 var FoellingerSchema = new mongoose.Schema({
     timestamp: { type: Number, default: 0 },
-    count: { type: Number, default: 0 }
+    count: { type: Number, default: 0 },
+    day: { type: Number, default: 0 },
+    date: { type: Number, default: 0 },
+    month: { type: Number, default: 0 },
+    year: { type: Number, default: 0 },
 });
 
 // Export the Mongoose model
